be73b165966b3f0a0d3d17ddd005bb6aa2945374
femmebot <femmebot@gmail.com>
2015-06-22 12:16:04 -0400

    used list copy

--- lists_2.py
+++ lists_2.py
@@ -3,11 +3,11 @@ the_list = ["a", 2, 3, 1, False, [1, 2, 3]]
 # Your code goes below here
 the_list.insert(0,the_list.pop(3))
 
-for item in the_list:
+the_list_copy = the_list[:]
+
+for item in the_list_copy:
     if type(item) is not int:
         the_list.remove(item)
-
-del the_list[len(the_list)-1]
 
 the_list.extend(range(4,21))
 
