4ae28ec9edf26c9f0b805a4364e07f8a3eac5864
femmebot <femmebot@gmail.com>
2015-06-24 18:49:23 -0400

    completed colections exrecises

--- collections.py
+++ collections.py
@@ -30,3 +30,25 @@ def first_and_last_4(my_list):
 
 first_and_last_4(clean_list)
 print("Combined: {}".format(first_and_last_4(clean_list)))
+
+
+# Exercises with deleting, replacing lists
+
+mixed_list = [1,2,'a','b','c','d',5,6,7,'f','g','h',8,9,'j']
+del mixed_list[:2]
+mixed_list[4:7] = ['e']
+mixed_list[8:10] = ['i']
+print(mixed_list)
+
+
+# The first half of the string, rounded with round(), should be lowercased.
+# The second half should be uppercased.
+# E.g. "Treehouse" should come back as "treeHOUSE"
+sample_string = 'Treehouse'
+
+def sillycase(my_string):
+    first_half_idx = round(len(my_string)/2)
+    return(my_string[:first_half_idx].lower()+my_string[first_half_idx:].upper())
+
+sillycase(sample_string)
+print (sillycase(sample_string))
