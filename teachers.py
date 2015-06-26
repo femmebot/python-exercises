1d5ab43db99e84d128da6c40cf378eca2e5a3694
femmebot <femmebot@gmail.com>
2015-06-26 00:52:44 -0400

    added dict exercises

---
+++ teachers.py
@@ -0,0 +1,10 @@
+# The dictionary will be something like:
+# {'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms', 'Technology Foundations'],
+#  'Kenneth Love': ['Python Basics', 'Python Collections']}
+#
+# Often, it's a good idea to hold onto a max_count variable.
+# Update it when you find a teacher with more classes than
+# the current count. Better hold onto the teacher name somewhere
+# too!
+#
+# Your code goes below here.
