2a42d58fa8970a6cea4e70ae8c673671dddc1002
femmebot <femmebot@gmail.com>
2015-06-29 22:04:12 -0400

    +game

---
+++ quest_game.py
@@ -0,0 +1,9 @@
+# Create a game with a 2-dimensional map.
+# Place the player, a door, and a monster into random spots in your map.
+# Let the player move around in the map and, after each move,
+# tell them if they've found the door or the monster.
+# If they find either the game is over.
+# The door is the win condition, the monster is the lose condition.
+
+def make_grid(row, col):
+    
