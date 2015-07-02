f884c58cc91f981fe65273b4b3c9c1a4f2724040
femmebot <femmebot@gmail.com>
2015-07-02 15:18:05 -0400

    add bounds checking

--- quest_game.py
+++ quest_game.py
@@ -8,10 +8,10 @@
 import random
 
 # make a 6x6 grid
-my_row = list('abcdef')
-my_col = list(range(1,7))
-coords = []
-my_move = list(input('> '))
+x_row = list(range(1,7))
+y_col = list(range(1,7))
+player = []
+# my_move = list(input('> '.upper()))
 
 def make_grid(row, col):
     grid_list = []
@@ -26,28 +26,71 @@ def generate_locations(coordinates):
     door = random.choice(my_coords)
     my_coords.remove(door)
     monster = random.choice(my_coords)
-    # print(my_coords)
-    # print(door, monster)
-    return(door, monster)
+    my_coords.remove(monster)
+    player_coords = random.choice(my_coords)
+    print(my_coords)
+    print(door, monster, player)
+    return(door, monster, player)
 
 def game_rules():
-    print("You're in a 6x6 room, with cells A-F across and 1-6 from top to bottom.")
-    print("You need to find the door by entering the secret cell coordinates, i.e., A1 for the top left cell. ")
+    print("You're in a 6x6 grid, with x 1 - 6 rows across and y 1 - 6 columns from top to bottom.")
+    print("You need to move UP, DOWN, LEFT or RIGHT to find the door.")
     print("But, be careful and don't enter the coordinates for the monster instead!")
-    print("Type HELP to view these rules, QUIT to end the game.")
+    print("Type HELP to view these instructions, QUIT to end the game.")
+
+# Get player's current location
+# Check to see if location is at the edge, i.e., x or y is at 1 or 6
+# and only permit moves that are within bounds
+def check_bounds(player):
+    if player == (1, ):
+        left_flag = True
+    elif player == (6, ):
+        right_flag = True
+    if player == ( , 1):
+        up_flag = True
+    elif player == ( , 6):
+        down_flag = True
+    if left_flag and up_flag:
+        print('Your current location is: {}. Type RIGHT or DOWN to move.'.format(player))
+    if left_flag and down_flag:
+        print('Your current location is: {}. Type RIGHT or UP to move.'.format(player))
+    if right_flag and up_flag:
+        print('Your current location is: {}. Type LEFT or DOWN to move.'.format(player))
+    if right_flag and down_flag:
+        print('Your current location is: {}. Type LEFT or UP to move.'.format(player))
+    if left_flag:
+        print('Your current location is: {}. Type RIGHT, UP or DOWN to move.'.format(player))
+    if right_flag:
+        print('Your current location is: {}. Type LEFT, UP or DOWN to move.'.format(player))
+    if up_flag:
+        print('Your current location is: {}. Type LEFT, RIGHT or DOWN to move.'.format(player))
+    if down_flag:
+        print('Your current location is: {}. Type LEFT, RIGHT or UP to move.'.format(player))
+    else:
+        print('Your current location is: {}. Type LEFT, RIGHT, UP or DOWN to move.'.format(player))
 
 
-# error-check input
-# no more than 2 characters; require letter (A-F), then number (1-6)
-#
+
+
 
 
 # generate the coordinates for the game
-game_coords = make_grid(my_row, my_col)
+game_coords = make_grid(x_row, y_col)
 # print(make_grid(my_row, my_col))
 
 # generate the coordinates for the door and the monster
 generate_locations(game_coords)
+
+# If LEFT, y = -1. If RIGHT, y = +1
+# If UP, x = -1. If DOWN, x = +1
+# error-check input
+check_bounds(player)
+
+def move(your_move):
+    while your_move != 'QUIT':
+        if your_move == 'HELP':
+            game_rules()
+
 
 
 #######
