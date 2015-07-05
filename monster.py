a7b27e64349daf201e33074e2db77cbccad9684a
femmebot <femmebot@gmail.com>
2015-07-05 12:16:47 -0400

    updated monster

--- monster.py
+++ monster.py
@@ -39,3 +39,25 @@ class Store(object):
     close = 10
     def hours(self):
         return (print("We're open from {} to {}.".format(self.open, self.close)))
+
+
+class Zombie:
+    # def __init__(self, hit_points=5, weapon='sword', color='blue'):
+    #     self.hit_points = hit_points
+    #     self.weapon = weapon
+    #     self.color = color
+    #     self.sound = sound
+
+    def __init__(self, **kwargs):  # double-underscore is called a dunder
+        self.hitpoints = kwargs.get('hit_points', 5)
+        self.weapon = kwargs.get('weapon', 'sword')
+        self.color = kwargs.get('color', 'blue')
+        self.sound = kwargs.get('sound', 'roar')
+    def battlecry(self):
+        return self.sound.upper()
+
+# >>> from monster import Zombie
+# >>> flag_zombie = Zombie(weapon='flag')
+# >>> flag_zombie.battlecry()
+# 'ROAR'
+# >>>
