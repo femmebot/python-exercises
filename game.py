adbb5829ffb80cb9dbf67a519965673f7bf2f343
femmebot <femmebot@gmail.com>
2015-07-06 19:55:25 -0400

    +comments

--- game.py
+++ game.py
@@ -12,3 +12,20 @@ class Game(object):
             Troll(),
             Dragon()
         ]
+        self.monster = self.get_next_monster()
+
+    def get_next_monster(self):
+        try:
+            return self.monsters.pop(0)
+        except IndexError:  # handles case where list is empty
+            return None
+
+    def monster_turn(self):
+        # check to see if mosnter attacks
+        # if so, tell the player
+            # Check if the player wants to dodge
+            # If so, check whether dodging was successful
+                # If it is, move on
+                # If not, remove one player hit point
+            # If the monster isn't attacking, inform the player
+            
