2ee432517651e50c3dc5755609ab650d8b30c754
femmebot <femmebot@gmail.com>
2015-07-09 20:19:26 -0400

    + pdb tracing

--- starter.py
+++ starter.py
@@ -6,7 +6,13 @@ logging.basicConfig(filename='starter.log', level=logging.DEBUG)
 # Add a docstring to Treehouse.student.
 # It should say "Gives a pleasant message about the student."
 
+import pdb; pdb.set_trace()
+# position at the point where you want to track activity
+# acceptable non-PEP8 format since we need to remove the line to run the script normally
+
 class Treehouse:
     def student(self, name):
         """Gives a pleasant message about the student."""
+        logging.info('Sample debug message')
+        # logging.debug('Sample message here')
         return '{} is a great Treehouse student!'.format(name)
